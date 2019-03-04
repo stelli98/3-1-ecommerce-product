af803f2478627da5177678c09c9a8f4374fa5586
stelli98_ <stellitan98@gmail.com>
2019-03-04 16:09:59 +0700

    Initialize Project

---
+++ src/main/java/com31/websiteecommerce/websiteecommerce/product/model/Product.java
@@ -0,0 +1,49 @@
+package com31.websiteecommerce.websiteecommerce.product.model;
+
+public class Product {
+    private Long id ;
+    private String name;
+    private String category;
+    private Long price;
+    private int qty;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+}
