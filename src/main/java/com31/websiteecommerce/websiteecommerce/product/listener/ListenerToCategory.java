81af59f4508fbd489b52387134355b5fe966b3f5
stelli98_ <stellitan98@gmail.com>
2019-03-13 13:25:44 +0700

    Kafka Added

---
+++ src/main/java/com31/websiteecommerce/websiteecommerce/product/listener/ListenerToCategory.java
@@ -0,0 +1,22 @@
+package com31.websiteecommerce.websiteecommerce.product.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com31.websiteecommerce.websiteecommerce.product.model.Category;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ListenerToCategory {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "category-product")
+    public void ListenerToCategory(String body) throws Exception {
+        Category category = objectMapper.readValue(body, Category.class);
+        log.info("Receive new category : {}", category.getName());
+    }
+}
